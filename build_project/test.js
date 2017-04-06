c625afa8b4aabaf17ceeda120cccd0b5863f2d49
lujh1 <lujh1@servyou.com.cn>
2017-04-06 14:04:57 +0800

    add menuApp

--- build_project/test.js
+++ build_project/test.js
@@ -1,9 +1,19 @@
-var i = 1
-
-console.log(/[^01]/.test('1233 546'))
-
-
-
-console.log('one 100 200'.match(/\d+/))
-
-console.log(Date.now())
+function likes(names) {
+  // TODO
+  var result
+  switch(names.length) {
+      case 0:
+      result = 'no one likes this'
+      break
+      case 1:
+      result = names[0] + ' likes this'
+      break
+      case 2:
+      result = names[0] + ' and ' + names[1] + ' like this'
+      break
+      case 3:
+      result = names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'
+      default:
+      result = names[0] = ', ' + names[1] + ' and ' + names.length + ' others like this'
+   }
+}
